808b732c8a67de4e2fbbd51ee5fa0e202f5f108d
ZekiAhmed <changezworld3@gmail.com>
2024-10-09 15:49:20 -0700

    add jsonwebtoken logic

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User from "../models/user.model.js";
+import CryptoJS from "crypto-js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 //REGISTER
@@ -7,7 +9,10 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
@@ -17,4 +22,38 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OrginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OrginalPassword !== req.body.password &&
+      res.status(401).json("wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 export default router;
