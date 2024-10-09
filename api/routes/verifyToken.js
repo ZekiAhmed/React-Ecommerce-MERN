046a3940626653409215aa736a708dbce02aa07a
ZekiAhmed <changezworld3@gmail.com>
2024-10-09 16:42:43 -0700

    create verifyToken file

---
+++ api/routes/verifyToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.header.token;
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
