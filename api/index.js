000015e459680efedb14151b45302fc34639ebba
ZekiAhmed <changezworld3@gmail.com>
2024-10-17 16:36:37 -0700

    added routes to index file

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import productRoute from "./routes/product.route.js";
+import cartRoute from "./routes/cart.route.js";
+import orderRoute from "./routes/order.route.js";
 
 const app = express();
 dotenv.config();
@@ -17,6 +19,8 @@ mongoose
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend Server is runing");
